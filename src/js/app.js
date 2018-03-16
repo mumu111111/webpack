c48c883c4f73f14ab1094de35cb99530d3aead41
mumu111111 <2646709035@qq.com>
2018-03-17 00:47:31 +0800

    init

---
+++ src/js/app.js
@@ -0,0 +1,7 @@
+import x from './m1'
+import y from './m2'
+import '../css/main.scss'
+
+x()
+console.log('hello, webpack')
+y()
---
+++ webpack.config.js
@@ -0,0 +1,43 @@
+const path = require('path');
+const ExtractTextPlugin = require('extract-text-webpack-plugin')
+
+
+module.exports = {
+  entry: './src/js/app.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist/js'),
+    // publicPath: 'dist'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['env']
+          }
+        }
+      },
+      {
+        test: /\.scss$/,
+        use: [{
+          loader: "style-loader" // creates style nodes from JS strings
+        }, {
+          loader: "css-loader", // translates CSS into CommonJS
+         
+        }, {
+          loader: 'postcss-loader',
+          options: { 
+            importLoaders: 1 ,
+            module:true
+          },
+        },{
+          loader: "sass-loader" // compiles Sass to CSS
+        }]
+      }
+    ]
+  }
+}
